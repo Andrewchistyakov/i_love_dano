ae5c82be3ac7f0e0b34965292454153b85fd5ce4
BlackfireZZZ <Stipundrik@gmail.com>
2025-11-16 21:10:34 +0300

    RAG

---
+++ RAG/Степа решение/toc.py
@@ -0,0 +1,187 @@
+toc = {
+    "CHAPTER 1": {
+        "title": "Introduction to Psychology",
+        "sections": {
+            "Introduction": 7,
+            "What Is Psychology?": 8,
+            "History of Psychology": 9,
+            "Contemporary Psychology": 18,
+            "Careers in Psychology": 26,
+        }
+    },
+    "CHAPTER 2": {
+        "title": "Psychological Research",
+        "sections": {
+            "Introduction": 35,
+            "Why Is Research Important?": 36,
+            "Approaches to Research": 41,
+            "Analyzing Findings": 48,
+            "Ethics": 59,
+        }
+    },
+    "CHAPTER 3": {
+        "title": "Biopsychology",
+        "sections": {
+            "Introduction": 71,
+            "Human Genetics": 72,
+            "Cells of the Nervous System": 78,
+            "Parts of the Nervous System": 84,
+            "The Brain and Spinal Cord": 86,
+            "The Endocrine System": 97,
+        }
+    },
+    "CHAPTER 4": {
+        "title": "States of Consciousness",
+        "sections": {
+            "Introduction": 109,
+            "What Is Consciousness?": 110,
+            "Sleep and Why We Sleep": 114,
+            "Stages of Sleep": 117,
+            "Sleep Problems and Disorders": 121,
+            "Substance Use and Abuse": 126,
+            "Other States of Consciousness": 134,
+        }
+    },
+    "CHAPTER 5": {
+        "title": "Sensation and Perception",
+        "sections": {
+            "Introduction": 145,
+            "Sensation versus Perception": 146,
+            "Waves and Wavelengths": 149,
+            "Vision": 153,
+            "Hearing": 161,
+            "The Other Senses": 164,
+            "Gestalt Principles of Perception": 168,
+        }
+    },
+    "CHAPTER 6": {
+        "title": "Learning",
+        "sections": {
+            "Introduction": 181,
+            "What Is Learning?": 182,
+            "Classical Conditioning": 183,
+            "Operant Conditioning": 192,
+            "Observational Learning (Modeling)": 203,
+        }
+    },
+    "CHAPTER 7": {
+        "title": "Thinking and Intelligence",
+        "sections": {
+            "Introduction": 213,
+            "What Is Cognition?": 214,
+            "Language": 218,
+            "Problem Solving": 222,
+            "What Are Intelligence and Creativity?": 228,
+            "Measures of Intelligence": 231,
+            "The Source of Intelligence": 237,
+        }
+    },
+    "CHAPTER 8": {
+        "title": "Memory",
+        "sections": {
+            "Introduction": 247,
+            "How Memory Functions": 248,
+            "Parts of the Brain Involved with Memory": 255,
+            "Problems with Memory": 259,
+            "Ways to Enhance Memory": 269,
+        }
+    },
+    "CHAPTER 9": {
+        "title": "Lifespan Development",
+        "sections": {
+            "Introduction": 279,
+            "What Is Lifespan Development?": 280,
+            "Lifespan Theories": 284,
+            "Stages of Development": 292,
+            "Death and Dying": 313,
+        }
+    },
+    "CHAPTER 10": {
+        "title": "Emotion and Motivation",
+        "sections": {
+            "Introduction": 321,
+            "Motivation": 322,
+            "Hunger and Eating": 328,
+            "Sexual Behavior": 334,
+            "Emotion": 342,
+        }
+    },
+    "CHAPTER 11": {
+        "title": "Personality",
+        "sections": {
+            "Introduction": 359,
+            "What Is Personality?": 360,
+            "Freud and the Psychodynamic Perspective": 362,
+            "Neo-Freudians: Adler, Erikson, Jung, and Horney": 368,
+            "Learning Approaches": 373,
+            "Humanistic Approaches": 377,
+            "Biological Approaches": 378,
+            "Trait Theorists": 379,
+            "Cultural Understandings of Personality": 384,
+            "Personality Assessment": 386,
+        }
+    },
+    "CHAPTER 12": {
+
+        "title": "Social Psychology",
+        "sections": {
+            "Introduction": 399,
+            "What Is Social Psychology?": 400,
+            "Self-presentation": 406,
+            "Attitudes and Persuasion": 409,
+            "Conformity, Compliance, and Obedience": 415,
+            "Prejudice and Discrimination": 422,
+            "Aggression": 429,
+            "Prosocial Behavior": 432,
+        }
+    },
+    "CHAPTER 13": {
+        "title": "Industrial-Organizational Psychology",
+        "sections": {
+            "Introduction": 447,
+            "What Is Industrial and Organizational Psychology?": 448,
+            "Industrial Psychology: Selecting and Evaluating Employees": 456,
+            "Organizational Psychology: The Social Dimension of Work": 467,
+            "Human Factors Psychology and Workplace Design": 477,
+        }
+    },
+    "CHAPTER 14": {
+        "title": "Stress, Lifestyle, and Health",
+        "sections": {
+            "Introduction": 485,
+            "What Is Stress?": 486,
+            "Stressors": 496,
+            "Stress and Illness": 502,
+            "Regulation of Stress": 514,
+            "The Pursuit of Happiness": 521,
+        }
+    },
+    "CHAPTER 15": {
+        "title": "Psychological Disorders",
+        "sections": {
+            "Introduction": 537,
+            "What Are Psychological Disorders?": 538,
+            "Diagnosing and Classifying Psychological Disorders": 542,
+            "Perspectives on Psychological Disorders": 545,
+            "Anxiety Disorders": 548,
+            "Obsessive-Compulsive and Related Disorders": 554,
+            "Posttraumatic Stress Disorder": 558,
+            "Mood and Related Disorders": 560,
+            "Schizophrenia": 570,
+            "Dissociative Disorders": 574,
+            "Disorders in Childhood": 576,
+            "Personality Disorders": 582,
+        }
+    },
+    "CHAPTER 16": {
+        "title": "Therapy and Treatment",
+        "sections": {
+            "Introduction": 599,
+            "Mental Health Treatment: Past and Present": 600,
+            "Types of Treatment": 605,
+            "Treatment Modalities": 617,
+            "Substance-Related and Addictive Disorders: A Special Case": 621,
+            "The Sociocultural Model and Therapy Utilization": 623,
+        }
+    },
+}
